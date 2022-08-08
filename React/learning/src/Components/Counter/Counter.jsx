e531c182b1cf6b1b71d14d6377ace4826549404b
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-09 03:06:12 +0530

    reusable

--- React/learning/src/Components/Counter/Counter.jsx
+++ React/learning/src/Components/Counter/Counter.jsx
@@ -15,14 +15,21 @@ const Counter = () => {
     if(count<=0){return} 
     setCount(count-1)
   }
+
+// q-2  just making a resuable function.
+  const handleOperation = (operation) => {
+    //   count=10 ? "reached" : setCount(count-1);
+    //   count=0 ? "reached" : setCount(count+1);
+      setCount(count + operation);
+  }
   return (
     <div>
         <div>Counter :{count} </div>
-        {/* <button onClick={handleAdd}> + 1 </button>
-        <button onClick={handleSub}> - 1 </button> */}
+        {/* <button onClick={handleAdd}> + 1 </button> */}
+        {/* <button onClick={handleSub}> - 1 </button> */}
 
-        <button onClick={handleSub}> - 1 </button>
-        <button onClick={handleAdd}> + 1 </button>
+        <button onClick={()=> {handleOperation(1)}}> add 1 </button>
+        <button onClick={()=> {handleOperation(-1)}}> sub 1 </button>
 
     </div>
   )
@@ -30,9 +37,9 @@ const Counter = () => {
 
 export default Counter;
 
-
+// q-1)
 // condition
 // stop at 0 
 // and it should not go beyond 10
 
-//Now make it resuable using one function only
+//  q-2) Now make it resuable using one function only
