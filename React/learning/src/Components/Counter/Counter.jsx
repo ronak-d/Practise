ad239930013101d700f1d221068c86c03e3c8e13
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-09 02:10:18 +0530

    Counter

--- React/learning/src/Components/Counter/Counter.jsx
+++ React/learning/src/Components/Counter/Counter.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 
+// to print UI of counter we need to use function from react
+// that is {useState} has [a,b] = useState( Accepts Intial value);
+
 const Counter = () => {
-  [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div>
         <div>Counter :{count} </div>
-        <button onClick={setCount}> + 1 </button>
-        <button onClick={setCount}> - 1 </button>
+        <button onClick={()=>{setCount(count+1)}}> + 1 </button>
+        <button onClick={()=>{setCount(count-1)}}> - 1 </button>
     </div>
   )
 }
