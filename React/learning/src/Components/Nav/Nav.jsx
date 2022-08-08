ebe8e5283710b2d54661a32f54f51f5b0aa05b4b
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-08 17:26:27 +0530

    Learing mapping

--- React/learning/src/Components/Nav/Nav.jsx
+++ React/learning/src/Components/Nav/Nav.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from "./Nav.module.css";
 
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
 const Nav = () => {
+
   return (
     <div>
+      {/* To iterate over an array we have to use {} brackets.*/}
+      {/* and to write Js in react we use {} also */}
 
+      {array.map((e) => {
+        <div>{el}</div>
+      })}
     </div>
   )
 }
 
-export default Nav
+export default Nav;
