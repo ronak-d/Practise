44355b3a6c37ab273ce215edfb64687e4b1bf3ae
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-09 02:32:59 +0530

    reusable counter

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Counter from "./Components/Counter/Counter";
 import Nav from "./Components/Nav/Nav"
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Counter/>
     </div>
   );
 }
--- React/learning/src/Components/Counter/Counter.jsx
+++ React/learning/src/Components/Counter/Counter.jsx
@@ -1,18 +1,38 @@
 import React, { useState } from 'react'
-
+// to make the clean code write js part upside and try to write html things in return part
 // to print UI of counter we need to use function from react
 // that is {useState} has [a,b] = useState( Accepts Intial value);
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleAdd = () =>{
+    if(count>=10){return} 
+    setCount(count+1)
+  }
+
+  const handleSub = () =>{
+    if(count<=0){return} 
+    setCount(count-1)
+  }
   return (
     <div>
         <div>Counter :{count} </div>
-        <button onClick={()=>{setCount(count+1)}}> + 1 </button>
-        <button onClick={()=>{setCount(count-1)}}> - 1 </button>
+        {/* <button onClick={handleAdd}> + 1 </button>
+        <button onClick={handleSub}> - 1 </button> */}
+
+        <button onClick={handleSub}> - 1 </button>
+        <button onClick={handleAdd}> + 1 </button>
+
     </div>
   )
 }
 
 export default Counter;
+
+
+// condition
+// stop at 0 
+// and it should not go beyond 10
+
+//Now make it resuable using one function only
