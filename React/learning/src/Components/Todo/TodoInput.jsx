9308b6002f09c80f8b07572504c6aaf6f7f05412
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-11 02:25:41 +0530

    added html for react

--- React/learning/src/Components/Todo/TodoInput.jsx
+++ React/learning/src/Components/Todo/TodoInput.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+
+// Input here means that what kind of html format required in the todo application;
+
+
 const TodoInput = () => {
   return (
-    <div>TodoInput</div>
+    <div>
+        <input type="text" />
+        <button>Add Todo</button>
+    </div>
   )
 }
 
-export default TodoInput
+export default TodoInput;
