e23d83e0def4f1b03eacb0cd51752146dfd8d9aa
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-11 02:31:11 +0530

    maintained usestate

--- React/learning/src/Components/Todo/TodoInput.jsx
+++ React/learning/src/Components/Todo/TodoInput.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 // Input here means that what kind of html format required in the todo application;
 
 
 const TodoInput = () => {
+
+    [SingleTodo, setSingleTodo] = useState("");
+
   return (
     <div>
         <input type="text" />
