f0be40b76a4d54d78419cde51171fa1ccc9f8059
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-16 23:32:00 +0530

    import useffect

--- learning/src/Components/useeffect/Learning.jsx
+++ learning/src/Components/useeffect/Learning.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { useEffect } from 'react';
-
+import { useEffect,useState } from 'react';
 
 // use of UseEffect.
+
 // useEffcts accepts 2 arguments/parameters. where 2nd one is optional (dependencies in life cycle methods [] ).
 // useEffects tells react to render at end of function returning.
 // if no dependency given will be render at every time.
 // if given [] = empty array will be render at mounting phase.
 // if given [state] = an array having state will only render when that state made some changes.
 
-
-
-
 const Learning = () => {
   return (
-    <div></div>
+    <div>
+        const [count,setCount] = useState(0)
+    </div>
   )
 }
 
