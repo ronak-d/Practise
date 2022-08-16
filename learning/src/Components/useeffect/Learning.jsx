e1f11b795e0b1114478b3959a6ed21b88f6396d6
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-16 23:23:25 +0530

    useEffect done

--- learning/src/Components/useeffect/Learning.jsx
+++ learning/src/Components/useeffect/Learning.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { useEffect } from 'react';
 
 
-// use of UseEffect;
-// useEffcts accepts 2 arguments/parameters. where 2nd one is optional (dependencies in life cycle methods [] );
-// 
+// use of UseEffect.
+// useEffcts accepts 2 arguments/parameters. where 2nd one is optional (dependencies in life cycle methods [] ).
+// useEffects tells react to render at end of function returning.
+// if no dependency given will be render at every time.
+// if given [] = empty array will be render at mounting phase.
+// if given [state] = an array having state will only render when that state made some changes.
 
 
 
