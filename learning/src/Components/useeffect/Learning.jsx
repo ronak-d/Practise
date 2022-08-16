9d6558cef0ff04692a0b09af7992e39faf9bbe31
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-16 23:16:41 +0530

    use of UseEffect

--- learning/src/Components/useeffect/Learning.jsx
+++ learning/src/Components/useeffect/Learning.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+
+
+// use of UseEffect;
+// useffcts accepts 2 arguments/parameters.
+
+
 
 const Learning = () => {
   return (
