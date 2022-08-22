372650b350d46c15a07fe0718bb473a8771c99ec
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-23 02:43:59 +0530

    mock server

--- learning/src/Components/useeffect/Todo.jsx
+++ learning/src/Components/useeffect/Todo.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// this to do in the way of fetching with the JSON server (mock server).
 
 const Todo = () => {
 
+  const [todolist, setTodolist] = useState([]);
+  const [todo, setTodo] = useState("");
+
   return (
     <div>
-      
+      <input type="text" value={value} />     
+      <button>Add</button>
     </div>
   )
 }
