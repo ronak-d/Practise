7111859cabd8e98df552e3233aaedf8318bcc7d7
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-27 03:22:36 +0530

    axios ways

--- learning/src/Components/useeffect/Todo.jsx
+++ learning/src/Components/useeffect/Todo.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 // show result with rerender.
 
 const Todo = () => {
-
+  const [loading, setLoading] =useState(true);
   const [todolist, setTodolist] = useState([]);
   const [todo, setTodo] = useState("");
   const [page,setPage] = useState(1);
@@ -29,17 +29,34 @@ const Todo = () => {
   useEffect(() =>{
   // axios GET request to receive todo from the server.
   // http://localhost:3000/users?_page=2&_limit=4
+    
+    setLoading(true);
 
-    axios.get(`http://localhost:3000/users?_page=${page}&_limit=5`)
+    // axios.get(`http://localhost:3000/users?_page=${page}&_limit=5`)      // 1=> this way.
+    
+    axios("http://localhost:3000/users",{     //-
+      method: "GET",                          //
+      params: {                               //
+        _page: page,                          //                            // 2=> Second way for axios using params
+        _limit:5                              //
+      }                                       //
+    })                                        //-
     .then((response) =>{
+      setLoading(false);
       console.log(response.data);
       setTodolist(response.data);
+    })
+    .catch((error) =>{
+      console.log(error);
+      setLoading(false);
     })
     
   },[page])
 
   return (
     <>
+      {loading && <div>Loading... Please Wait</div>}
+
       <div>
         <input type="text" onChange={(e) => setTodo(e.target.value)}/>     
         <button onClick={() => handleTodo(todo)}>Add</button>
@@ -50,7 +67,7 @@ const Todo = () => {
       </div>
 
       <div>
-        <button onClick={() => setPage(page-1)}>prev</button>
+        <button disabled={page<1} onClick={() => setPage(page-1)}>prev</button>
         <button onClick={() => setPage(page+1)}>Next</button>
       </div>
     </>
