bb4f60dac35de31df3faadd2fd054311ec0843e5
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-27 14:25:16 +0530

    phase of Un-Mounting

--- learning/src/Components/useeffect/CleanUpFunction.jsx
+++ learning/src/Components/useeffect/CleanUpFunction.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+
+
+// Clean-ups are the things which are done by the React-Dom in the phase of Un-Mounting(removing the element from the UI).
+
 const CleanUpFunction = () => {
-    
+
   return (
     <div>
 
