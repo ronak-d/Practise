54038b64d47631b8dcc57201142a8336483f5339
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-28 14:45:02 +0530

    memory leak

--- learning/src/Components/useeffect/CleanUpFunction.jsx
+++ learning/src/Components/useeffect/CleanUpFunction.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 // Clean-ups are the things which are done by the React-Dom in the phase of Un-Mounting(removing the element from the UI).
 
 const CleanUpFunction = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() =>{
+    let id = setInterval(()=>{
+
+      setCount((prevValue) => {      
+        if(prevValue <= 0){
+          clearingInterval(id);
+          return 0
+        }
+      });
+  
+    },1000)
+  },[]);
 
   return (
-    <div>
-
-    </div>
+    () => clearingInterval(id)
   )
 }
 
