f3eba2f8bbb68ecc5349ef51cfdadb2c4f9c96af
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-29 11:57:14 +0530

    clean ups

--- learning/src/Components/useeffect/CleanUpFunction.jsx
+++ learning/src/Components/useeffect/CleanUpFunction.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react'
 
-
-
 // Clean-ups are the things which are done by the React-Dom in the phase of Un-Mounting(removing the element from the UI).
 // Clean-ups used to enhance optimisation, Prevents Memory Leak and Cache.
 
 // setInterval => to stop it we use clearInterval.
 
 const CleanUpFunction = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(5);
 
   useEffect(() =>{
     let id = setInterval(()=>{
@@ -23,11 +21,19 @@ const CleanUpFunction = () => {
     },1000)
   },[]);
 
+
+  // this return is a Clean-Ups performed by react.
+  // return ()=> {
+  //   clearInterval(id);
+  // };
+
   return (
-    () => clearInterval(id)
+    <>
+    <h3>Count : {count}</h3>
+    </>
   )
 }
 
 export default CleanUpFunction;
 
-// 1:40
+// 1:30
