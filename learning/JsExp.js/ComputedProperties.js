93d1dc7e0a95dd920b119f8c6d26a93a217fc9cf
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-31 03:07:08 +0530

    New learning jsexp.js

---
+++ learning/JsExp.js/ComputedProperties.js
@@ -0,0 +1,11 @@
+// Computed Properties.
+
+var x = 2;
+
+// we are calling that -> data of x which exits as 2 now will be 5 ;
+
+let data = {
+    [x] : 5  // -> { '2': 5 }
+}
+
+console.log(data);
