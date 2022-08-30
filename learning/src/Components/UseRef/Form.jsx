8ef0213025dc99e61381a8691488b782491409b7
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-31 02:38:58 +0530

    Useref only renders the part which needs a render.

--- learning/src/Components/UseRef/Form.jsx
+++ learning/src/Components/UseRef/Form.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 const Form = () => {
+
+    const [formData, setFormData] =useState({
+        username:"",
+        age:"",
+        ops:false
+    })
+
+    function handleChange(event){
+
+        setFormData({
+            ...formData,
+            [event.target.id] : event.target.value
+        })
+
+    }
+
   return (
-    <div>Form</div>
+
+    <div>
+        {/*  */}
+        <input onChange={handleChange} id="username" type="text" placeholder="Enter Username" />
+        <input onChange={handleChange} id="age" type="number"placeholder="Enter age" />
+        <button onclick={()=>handleChange}>save</button>
+    </div>
   )
 }
 
-export default Form
+export default Form;
