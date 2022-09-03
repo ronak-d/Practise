3ae7a610d67edd1a210b59af0ce9bd943cac9ec2
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-03 15:00:14 +0530

    Routers in react

---
+++ learning/src/Components/Routers/Route.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Route = () => {
+  return (
+    <div>
+        Hello World Today we are learning about Routings
+        1st Install react-router-dom library from react.
+    </div>
+  )
+}
+
+export default Route
