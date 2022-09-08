8c3fcb78135c037306b7e3fce3f4c30933dddd03
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-08 16:16:32 +0530

    Routers

---
+++ learning/src/Components/Routers/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <button>Rotes Info</button>
+        <button>Home</button>
+        <button>About</button>
+    </div>
+  )
+}
+
+export default Navbar
--- learning/src/Components/Routers/Route.jsx
+++ learning/src/Components/Routers/Route.jsx
@@ -8,6 +8,10 @@ const RoutesDemo = () => {
         <h3>2nd we have to enable the router in our entire App/Index.js -- "BrowserRouter".</h3>
         <hr />
         <h3>import BrowserRouter from 'react-router-dom';</h3>
+        <hr />
+        <h3>Maintain Routes wrapper with each Route in it. With all closing tags.</h3>
+        <hr />
+        <h3>Give Link to Navbar to navigate to ceratin page.</h3>
     </div>
   )
 }
