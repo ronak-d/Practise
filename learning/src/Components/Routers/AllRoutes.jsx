74fbaec0cabb23a874aa4fe61661f4cb6d9ac090
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-17 03:05:49 +0530

    use : colon for dynamic routing

--- learning/src/Components/Routers/AllRoutes.jsx
+++ learning/src/Components/Routers/AllRoutes.jsx
@@ -12,22 +12,29 @@ const AllRoutes = () => {
   return (
     <>
     <Navbar/>
+    
     <Routes>
         <Route path="/" element={<AppRoutes/>}/>
         <Route path="/About" element={<About/>}/>
         <Route path="/Homey" element={<Homey/>}/>
         <Route path="/UsersList" element={<UsersList/>}/>
-        <Route path="/UsersList/id" element={<UserDetails/>}/>
+        <Route path="/UsersList/:mail" element={<UserDetails/>}/>
     </Routes>
     </>
   )
 }
 
 export default AllRoutes;
-// continue at 1:36 mins
 // Routes is a wrapper where each route has to be defined. 
 // path = triggers element 
 // element + Each Route has to be closed before
 
 
 // be very careull in the type error if typed (component or elements) causing error =>Matched leaf route at location "/" does not have an element. This means it will render an with a null value by default resulting in an "empty" page
+
+
+{/* <Route path="/UsersList/id" element={<UserDetails/>}/>  */} 
+// if i give any param after {Userlist} it has to trigger element of {UserDetails}
+
+// IMP.
+// always use [ : ] for dynamic routing (routing inside routig concepts).
--- learning/src/Components/Routers/UserDetails.jsx
+++ learning/src/Components/Routers/UserDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const UserDetails = () => {
+  const {mail} = useParams();
   return (
+    <>
     <div>UserDetails</div>
+    <h1>hello</h1>
+    </>
   )
 }
 
 export default UserDetails
+
+
+
+// we can here use UseParams hook for anything passed inside the path param
+// will passed as prop and we can use them as a prop.
