f261c360f19690118e33856ea889b93b21561fa1
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-16 16:02:29 +0530

    mapping of users list

--- learning/src/Components/Routers/UsersList.jsx
+++ learning/src/Components/Routers/UsersList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UsersList = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users')
+    .then((response) => setUsers(response.data.data))
+    .catch((error) => console.error(error));
+  },[]);
+  
+  console.log(users);
   return (
-    <h3>UsersList</h3>
+    <>
+      <h3>UsersList</h3>
+      
+      {users.map((user) =>{
+        <p> {users.id}. {users.first_name}</p>
+      })}
+    </>
   )
 }
 
 export default UsersList
+
+
+// call users from reqres users.
+// https://reqres.in/api/users
