7da37adeeb60b1b23cbf3ab2d82059282dcb9e00
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-19 06:45:13 +0530

    reqres password

---
+++ learning/src/Components/AuthContext/AuthContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react'
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({children}) => {
+
+  return (
+    <AuthContext.Provider value = {}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider;
